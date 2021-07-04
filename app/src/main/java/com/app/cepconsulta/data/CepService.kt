4cd19bbe67e1fb749ab4de744aca8580d74c7f9b
luizgustavobrum <luizgustavo.brum@gmail.com>
2021-07-04 20:06:13 -0300

    BaseUrl como constante

--- app/src/main/java/com/app/cepconsulta/data/CepService.kt
+++ app/src/main/java/com/app/cepconsulta/data/CepService.kt
@@ -4,12 +4,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object CepService {
+
+    const val BASE_URL = "https://viacep.com.br/ws/"
+
     fun createCepServiceRetrofit(): Retrofit = Retrofit
         .Builder()
-        .baseUrl("https://viacep.com.br/ws/")
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val cepServiceInterfaceImpl = createCepServiceRetrofit().create(CepServiceInterface::class.java)
+    val cepServiceInterfaceImpl: CepServiceInterface = createCepServiceRetrofit().create(CepServiceInterface::class.java)
 
 }
