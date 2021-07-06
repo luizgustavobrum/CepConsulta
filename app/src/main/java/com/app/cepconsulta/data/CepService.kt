7495dd0ed9ea57a90640e3e7344a95e4e33c3833
luizgustavobrum <luizgustavo.brum@gmail.com>
2021-07-06 15:02:02 -0300

    Adição do OkHttpClient

--- app/src/main/java/com/app/cepconsulta/data/CepService.kt
+++ app/src/main/java/com/app/cepconsulta/data/CepService.kt
@@ -1,18 +1,28 @@
 package com.app.cepconsulta.data
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object CepService {
 
     const val BASE_URL = "https://viacep.com.br/ws/"
+    const val TIME_OUT: Long = 30
+
+    fun createOkHttpClient() = OkHttpClient.Builder()
+        .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+        .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+        .build()
 
     fun createCepServiceRetrofit(): Retrofit = Retrofit
         .Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(createOkHttpClient())
         .build()
 
-    val cepServiceInterfaceImpl: CepServiceInterface = createCepServiceRetrofit().create(CepServiceInterface::class.java)
+    val cepServiceInterfaceImpl: CepServiceInterface =
+        createCepServiceRetrofit().create(CepServiceInterface::class.java)
 
 }
