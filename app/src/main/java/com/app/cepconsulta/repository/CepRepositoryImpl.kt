e884c1a1db4da5eb3bfb9e6970f0e4ae5d3d38c6
luizgustavobrum <luizgustavo.brum@gmail.com>
2021-07-06 15:02:52 -0300

    Adição de logs

--- app/src/main/java/com/app/cepconsulta/repository/CepRepositoryImpl.kt
+++ app/src/main/java/com/app/cepconsulta/repository/CepRepositoryImpl.kt
@@ -1,14 +1,21 @@
 package com.app.cepconsulta.repository
 
+import android.util.Log
 import com.app.cepconsulta.data.CepService.cepServiceInterfaceImpl
 import com.app.cepconsulta.mapper.toModel
 import com.app.cepconsulta.model.CepModel
 import com.app.cepconsulta.model.Result
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import java.lang.Exception
 
 class CepRepositoryImpl : CepRepository {
+
+    companion object {
+        private val TAG = CepRepositoryImpl::class.java.name
+    }
+
     override suspend fun getCep(
         cep: String,
         dispatcher: CoroutineDispatcher
@@ -21,7 +28,11 @@ class CepRepositoryImpl : CepRepository {
                 Result.Error(value = "Response is not successful or body is null")
             }
 
+        } catch (e: HttpException) {
+            Log.e(TAG, e.toString())
+            Result.Error(value = ">>>HttpException${e.toString()}")
         } catch (e: Exception) {
+            Log.e(TAG, e.toString())
             Result.Error(value = e.toString())
         }
     }
